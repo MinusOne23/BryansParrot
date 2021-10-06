a281b534ee8b381c7d8a54b27797b8443dba9ad7
Schimpfa <Schimpfa99@gmail.com>
2021-10-06 18:33:15 -0400

    Enemy File did not transfer over

---
+++ BryansParrot/Enemy.cpp
@@ -0,0 +1,9 @@
+#include "Enemy.h"
+#include <string>
+using namespace std;
+
+Enemy::Enemy(string _name)
+{
+	name = _name;
+
+}
---
+++ BryansParrot/Enemy.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <string>
+
+using namespace std;
+
+/*
+* Abstract Item class that all items in the game will
+* inherit from
+*
+* getDisplay() -- function that returns what the item should
+* display to the user when in the inventory or in a room
+*
+* name -- the name of the item for shorthand display
+*/
+class Enemy
+{
+public:
+	
+	inline string getEnemyName() const { return name; }
+	Enemy(string _name);
+
+protected:
+	string name ;
+};
+
+#endif // Enemy_H
+
